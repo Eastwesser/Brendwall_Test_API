595514e550f33ef854843152bdfbde2c08eff128
Dennis Mattews <alttert@mail.ru>
2024-09-13 21:59:28 +0300

    templates and urls refactoring

--- brendwall_tz/urls.py
+++ brendwall_tz/urls.py
@@ -23,5 +23,7 @@ from django.urls import path, include
 # ]
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
+    path('', include('products.urls')),  # добавляем маршруты products на корневой путь
 ]
